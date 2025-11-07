b952f5be663ed1b34d1007c98276a06a1bf5bd09
cascercata2014-cloud <cascercata.2014@gmail.com>
2025-11-07 15:32:17 -0500

    Add files via upload

---
+++ backend/assets/models/mensajes.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+/*const UsuarioSchema = new mongoose.Schema({
+    cedula:String,
+    userName:String,
+    nombre:String,
+    apellido:String,
+    correo:String,
+    password:String,
+    telefono: int 
+}, {collection:"usuarios"});*/
+
+const MensajeSchema = new mongoose.Schema({
+    mensaje:String
+});
+
+mongoose.model("mensajes",MensajeSchema)
